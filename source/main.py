944760507f0a95c2650e7bdf53dc4119cc7966c8
DJ FLOW <czyrt2008@gmail.com>
2022-02-19 11:06:23 +0100

    Dark design and button to change the design added.

--- source/main.py
+++ source/main.py
@@ -4,7 +4,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    theme_cookie = request.cookies.get("theme")
+    if theme_cookie is None:
+        return render_template("index.html")
+    elif theme_cookie == "light":
+        return render_template("index_light.html")
+    elif theme_cookie == "dark":
+        return render_template("index_dark.html")
+    elif theme_cookie == "auto":
+        return render_template("index.html")
+    else:
+        return "Error"
+
+@app.route('/upload')
+def upload():
+    return render_template("upload.html")
+
+@app.route("/media")
+def media():
+    if request.args.get("file") is None:
+        return render_template("404.html")
+    else:
+        try:
+            file = str("media/"+request.args.get("file"))
+            return send_file(file)
+        except:
+            return render_template("404.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
